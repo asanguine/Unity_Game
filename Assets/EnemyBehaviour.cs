755444a7e2effbccb295a4b4169ca549411b7d13
Birkan K <54071079+asanguine@users.noreply.github.com>
2024-01-25 14:19:36 +0100

    made enemy pass thruoable

--- Assets/EnemyBehaviour.cs
+++ Assets/EnemyBehaviour.cs
@@ -8,6 +8,7 @@ public class EnemyBehaviour : MonoBehaviour
     private Animator animator;
     private float speed;
     private int moveDirection;
+    public LayerMask passThroughLayers;
     void Start()
     {
         animator = gameObject.GetComponent<Animator>();
@@ -37,6 +38,12 @@ public class EnemyBehaviour : MonoBehaviour
         {
             gameObject.GetComponent<Transform>().rotation =
             Quaternion.Euler(new Vector3(0, 0, 0));
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (passThroughLayers == (passThroughLayers | (1 << collision.gameObject.layer))) {
+            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), collision.collider);
         }
     }
 }
