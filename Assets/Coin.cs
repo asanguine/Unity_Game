b8cb455d88944321d0210daf5a2cdbed5417313f
Birkan K <54071079+asanguine@users.noreply.github.com>
2024-01-25 14:08:58 +0100

    added coin pickup sfx

--- Assets/Coin.cs
+++ Assets/Coin.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     public static int coins;
+    public AudioSource coinSound;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     void OnTriggerEnter2D(Collider2D col)
@@ -17,6 +20,7 @@ public class Coin : MonoBehaviour
         if (col.CompareTag("Player"))
         {
             coins++;
+            coinSound.Play();
             Destroy(gameObject);
         }
     }
